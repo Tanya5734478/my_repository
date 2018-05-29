2d3ba747280eee28a8190588573e3b9451e7577d
Dmitry Tyunyaev <greenembrace@gmail.com>
2018-05-29 17:33:19 +0300

    Block filter created

---
+++ 2.js
@@ -0,0 +1,12 @@
+﻿var word = (process.argv[2]);
+var result = "";
+
+
+for (var i=0;i<word.length;i++) {
+        if (word[i]!="К" && word[i]!="к"&& word[i]!="Р"&& word[i]!="р"&& word[i]!="Н"&& word[i]!="н") {
+                result+=String(word[i]);
+        }
+}
+
+console.log(result);
+
